eb675c8d1e6eecefc0ba63b1cbef5b9ee89d7105
wuzhining <wzn354753575@sina.cn>
2019-10-04 11:57:06 +0800

    新加项目

---
+++ ssm/src/test/java/com/ssm/test/socket/SocketServer.java
@@ -0,0 +1,54 @@
+package com.ssm.test.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SocketServer {
+	public static ServerSocket serverSocket;
+	public static List<Map<String, Object>> clientSocket = new ArrayList();
+
+	public static void main(String[] args) throws IOException {
+		/**
+		 * 基于TCP协议的Socket通信，实现用户登录，服务端
+		 */
+		// 1、创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
+		ServerSocket serverSocket = new ServerSocket(10086);// 1024-65535的某个端口
+		// 2、调用accept()方法开始监听，等待客户端的连接
+		Socket socket = serverSocket.accept();
+		// 3、获取输入流，并读取客户端信息
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String info = null;
+		
+		OutputStream os = socket.getOutputStream();
+		PrintWriter pw = new PrintWriter(os);
+		while ((info = br.readLine()) != null) {
+			System.out.println("我是服务器，客户端说：" + info);
+			// 4、获取输出流，响应客户端的请求
+			pw.write("欢迎您！");
+			pw.flush();
+		}
+		socket.shutdownInput();// 关闭输入流
+		
+
+		// 5、关闭资源
+		pw.close();
+		os.close();
+		br.close();
+		isr.close();
+		is.close();
+		socket.close();
+		serverSocket.close();
+	}
+}
